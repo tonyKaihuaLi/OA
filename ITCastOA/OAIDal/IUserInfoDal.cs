aaa0dfaf132ab751b26d3af2a8b2eaa882c93d45
李凯华 <likaihua2016@gmail.com>
2019-01-09 01:50:57 -0500

    Frame

---
+++ ITCastOA/OAIDal/IUserInfoDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OAModel;
+
+namespace OAIDal
+{
+    public interface IUserInfoDal:IBaseDal<Book>
+    {
+
+    }
+}
